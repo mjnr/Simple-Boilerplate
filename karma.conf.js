641e28a71e33cb307004c69ebd0f715e6fe6bafe
Milson Júnior <junior.milson17@gmail.com>
2015-10-06 00:23:13 -0300

    Improving karma config

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,6 @@ module.exports = function(config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-browserify'),
-      require('karma-coverage'),
       require('karma-phantomjs-launcher')
    ],
 
@@ -33,19 +32,15 @@ module.exports = function(config) {
 	 // preprocess matching files before serving them to the browser
 	 // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 	 preprocessors: {
-      "tests/**/*.spec.js": ["browserify", "coverage"]
+     'tests/**/*.spec.js': ['browserify'],
 	 },
 
 
 	 // test results reporter to use
 	 // possible values: 'dots', 'progress'
 	 // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-	 reporters: ['progress', 'coverage'],
+	 reporters: ['progress'],
 
-   coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
-   },
 
 	 // web server port
 	 port: 9876,
--- tests/module.spec.js
+++ tests/module.spec.js
@@ -5,10 +5,17 @@
 var myTestableModule = require("../src/scripts/modules/my-testable-module.js");
 
 describe("Function to multiply two numbers", function(){
-   var arithmetic = myTestableModule;
+   var calculator = myTestableModule;
 
-   it("should return the result of the multiplication", function(){
-      expect(arithmetic).toBeDefined()
-      expect(arithmetic.mult(5,2)).toBe(10)
+   it("module should be defined", function(){
+     expect(calculator).toBeDefined()
+   });
+
+   it("should return the result of the multiplication 5 * 2", function(){
+      expect(calculator.mult(5,2)).toBe(10)
+   });
+
+   it("should return the result of the multiplication 2 * 3", function(){
+      expect(calculator.mult(2,3)).toBe(6)
    });
 });
