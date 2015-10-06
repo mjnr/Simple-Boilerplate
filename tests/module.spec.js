4b8a156f084d889c87608ef21bfd17669f130032
Milson Júnior <milson.junior@fbiz.com.br>
2015-10-06 09:23:21 -0300

    change jasmine matcher

--- tests/module.spec.js
+++ tests/module.spec.js
@@ -7,8 +7,15 @@ var myTestableModule = require("../src/scripts/modules/my-testable-module.js");
 describe("Function to multiply two numbers", function(){
    var arithmetic = myTestableModule;
 
-   it("should return the result of the multiplication", function(){
+   it("it should be defined", function(){
       expect(arithmetic).toBeDefined()
-      expect(arithmetic.mult(5,2)).toBe(10)
+   });
+
+   it("5 and 2 should return 10", function(){
+      expect(arithmetic.mult(5,2)).toEqual(10)
+   });
+
+   it("2 and 2 should return 4", function(){
+      expect(arithmetic.mult(2,8)).toEqual(16)
    });
 });
