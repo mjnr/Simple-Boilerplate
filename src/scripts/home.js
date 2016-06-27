e453b126fed6a00e77f450e9e3722ec36243c156
milson_junior <junior.milson17@gmail.com>
2016-06-27 01:38:09 -0300

    Removing old structure

--- src/scripts/home.js
+++
@@ -1,9 +0,0 @@
-/*
-* Home Javascript file.
-*/
-
-import { ZodiacSaint, BronzeSaint } from "./modules/my-module";
-
-let saintIkki = new BronzeSaint("Ikki", "Phoenix");
-saintIkki.sayName();
-// My name is Ikki and I am the Bronze Phoenix saint
