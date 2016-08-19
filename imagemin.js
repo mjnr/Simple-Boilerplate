056b6a6f2bd5585d5608e7e8da5e5b18df3f0c98
Victor Schulze Buchalla <vs_buchalla@hotmail.com>
2016-08-19 17:01:43 -0300

    adding imagemin-mozjpeg and imagemin-pngquant plugins

--- imagemin.js
+++ imagemin.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const imagemin = require('imagemin'),
+	imageminMozjpeg = require('imagemin-mozjpeg'),
+	imageminPngquant = require('imagemin-pngquant'),
 	path = require('path'),
 	glob = require('glob'),
 	conf = require('./package.json').config,
@@ -16,9 +18,14 @@ function optimizeImages(err, files) {
 	files.forEach((file) => {
 		const folder = path.dirname(file).replace(paths.dev.split('/*').shift(), paths.dist);
 
-		imagemin([file], folder);
+		imagemin([file], folder, {
+		    plugins: [
+		        imageminMozjpeg(),
+		        imageminPngquant({quality: '65-80'})
+		    ]
+		});
 	});
-	
+
 	console.log(`${files.length} minified images.`);
 }
 
